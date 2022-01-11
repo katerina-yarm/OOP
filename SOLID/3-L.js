afa754d09b0ac561152d41c1383e0d86035c4d15
katerina-yarm <katerina.tatarevich@gmail.com>
2022-01-11 15:19:34 +0300

    Liskov principle was realised

--- SOLID/3-L.js
+++ SOLID/3-L.js
@@ -1,2 +1,28 @@
-// if class A is a subtype of class B, we should be able to replace B with A without disrupting the behavior of our program
+// Liskov substitution - if class A is a subtype of class B, we should be able to replace B with A without disrupting the behavior of our program
+// The inheriting class should complete, not override, the behavior of the base class.
 
+class Member {
+  constructor(options) {
+    this.club = options.club
+  }
+  access() {
+    console.log(`I have access to private football club information as "${this.club}" member`)
+  }
+}
+
+class Player extends Member {
+  constructor(options) {
+    super(options)
+    this.name = options.name
+  }
+}
+
+const player = new Player({ club: 'Manchester United', name: 'Marcus Rashford' })
+const member = new Member({ club: 'Manchester United' })
+
+function getSecretInfo(member) {
+  member.access()
+}
+
+getSecretInfo(member)
+getSecretInfo(player)
